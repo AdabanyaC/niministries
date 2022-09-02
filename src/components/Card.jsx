de86610a8426579d18bf05566a7661a67590dc33
Clinton Adabanya <clintonadabanya@gmail.com>
2022-09-02 05:46:01 +0100

    Now fetching sermons from API

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Card = ({ title, description, footer, url }) => {
   return (
-    <div className="bg-white p-6 w-[378px] myShadow rounded-2xl mt-8">
-      <img src={url} alt="Supernatural Class Febuary" />
+    <div className="bg-white p-6 w-[420px] myShadow rounded-2xl mt-8">
+      <img
+        src={url}
+        alt="Supernatural Class Febuary"
+        className="h-[350px] w-[420px] rounded-lg"
+      />
       <h3 className="text-[28px] mt-4 font-bold"> {title} </h3>
       <p className="text-justify mt-4">{description}</p>
       <div className="mt-6">{footer}</div>
